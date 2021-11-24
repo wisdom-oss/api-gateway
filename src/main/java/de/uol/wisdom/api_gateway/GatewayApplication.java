a05bcafc23d60dc2a81759191004e919311b4ad8
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-11-24 14:18:53 +0100

    Added CORS filter for running the application locally

--- src/main/java/de/uol/wisdom/api_gateway/GatewayApplication.java
+++ src/main/java/de/uol/wisdom/api_gateway/GatewayApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -25,6 +29,18 @@ public class GatewayApplication {
 	@Order(0)
 	public GlobalFilter AuthorizationCheck() {
 		return new TokenValidationFilter();
+	}
+
+	@Bean
+	@Profile("local")
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.applyPermitDefaultValues();
+		configuration.addAllowedOrigin("*");
+		configuration.addAllowedMethod("*");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return new CorsWebFilter(source);
 	}
 
 }
