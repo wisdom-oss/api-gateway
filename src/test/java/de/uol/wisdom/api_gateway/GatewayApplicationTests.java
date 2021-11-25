32d14ef059d7b15571657b3b4408dd5034ed5316
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-11-25 18:34:20 +0100

    Added test for loading the configuration files and added local config

---
+++ src/test/java/de/uol/wisdom/api_gateway/GatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package de.uol.wisdom.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	/**
+	 * Test if the configuration of the application is valid.
+	 */
+	@Test
+	void contextLoads() {
+	}
+}
