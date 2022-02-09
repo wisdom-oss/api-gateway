21cb6a4f5c93354e27f4a15da58734a931dcb0e9
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-02-09 12:47:36 +0100

    Updated the documentation files (currently pure JavaDoc)

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.uol.wisdom.api_gateway","l":"GatewayApplication"},{"p":"de.uol.wisdom.api_gateway.filters","l":"RequestIDHeaderGeneration"},{"p":"de.uol.wisdom.api_gateway.filters","l":"TokenValidationFilter"}];updateSearchResults();
